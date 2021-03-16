cf031af82635718440a8c55fb03c8f3d6a560679
DragonVova <macrosmovie@gmail.com>
2021-03-16 21:41:34 +0200

    Update yarn.lock and migrations

--- src/migrations/1615828291179-InitialMigration.ts
+++ src/migrations/1615828291179-InitialMigration.ts
@@ -4,8 +4,8 @@ export class InitialMigration1615828291179 implements MigrationInterface {
   name = 'InitialMigration1615828291179';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(
-      `CREATE TABLE "user" ("id" SERIAL NOT NULL, "title" character varying NOT NULL, "age" integer NOT NULL, "height" integer, "description" text, "created_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`,}
+        await queryRunner.query(`CREATE TABLE "user" ("id" SERIAL NOT NULL, "title" character varying NOT NULL, "age" integer NOT NULL, "height" integer, "description" text, "created_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`);
+    }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`DROP TABLE "user"`);
